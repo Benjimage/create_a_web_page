d49d419033bef3f31a9f200e54bd6ead8db4963e
benjamin <benjbruant@gmail.com>
2024-08-26 21:32:44 +0200

    intégration de la fonction changeElement

--- script.js
+++ script.js
@@ -28,11 +28,25 @@ subtitle .addEventListener('click', ()=> {addTextElement(1,"h3", "sub", ".conten
 paragraph.addEventListener('click', ()=> {addTextElement(2,"p", null, ".new-div")})
 list.addEventListener('click', addList)
 button.addEventListener('click',addButton)
-/*size.addEventListener('click', )
-img.addEventListener('click', )
-typo.addEventListener('click', )
-color.addEventListener('click', )
- .addEventListener('click', ()=> {addElement("", "", "")}) */
+size.addEventListener('click', ()=>{document.querySelector('.conteneur').style.height = "88%"})
+img.addEventListener('click', addImg)
+typo.addEventListener('click',()=>{changeElement('h1','font-family',"Edu NSW ACT Foundation, cursive")})
+typo.addEventListener('click',()=>{changeElement('h1','font-size',"4rem")})
+color.addEventListener('click', ()=>{
+    const surprise = document.createElement('button')
+    surprise.classList.add('surprise')
+    surprise.setAttribute("type","button")
+    surprise.innerText = "Surprise"
+    surprise.addEventListener('click',fonctionSurprise)
+    document.querySelector(".conteneur").appendChild(surprise)
+
+    let noirEtblanc = document.querySelector('img')
+    noirEtblanc.classList.add('nb')
+    document.querySelector('.new-section').style.backgroundColor = "#008a88"
+    document.querySelector('.conteneur').style.backgroundColor = "#004a7f"
+    document.querySelector('.conteneur').style.color = "#fff"
+    document.querySelector('.new-div').style.color = "#001e33"
+})
 
 
 function addElement(element,classe,selector) {
@@ -54,6 +68,53 @@ function addList() {
         addTextElement(3,"li",'list',"ul")
     }
 }  
+function addButton() {
+    const newButton = document.createElement("button")
+    userInput = prompt(message[4])
+    newButton.innerText = userInput
+    newButton.setAttribute("type","button")
+    /* newButton.addEventListener('click',()=>{document.querySelector('.new-section').style.backgroundColor = "#004a7f"}) */
+    newButton.addEventListener('click', ()=> {changeElement('.new-section', "background-color","#004a7f")})
+    document.querySelector(".conteneur").appendChild(newButton)
+}
+function changeElement(selector,property,value){
+   document.querySelector(selector).style.setProperty(property,value);
+}
+function addImg() {
+    const newImg = document.createElement("img")
+    newImg.classList.add("vignette")
+    newImg.setAttribute("src","images/Hi.png")
+    newImg.setAttribute("id","image")
+    newImg.setAttribute("title","cheval qui rit !")
+    document.querySelector('.conteneur').appendChild(newImg)
+}
+function fonctionSurprise(){
+    document.querySelector('img').src='images/bou.png'
+    document.querySelector('img').style.height = "120px"
+    document.querySelector('.sub').style.display = "none"
+    document.querySelector('p').style.display = "none"
+    document.querySelector('.list').style.display = "none"
+    let newStyle = document.querySelector('img')
+    newStyle.classList.remove('nb')
+    let newTitle = document.querySelector(".up")
+    newTitle.innerText = "Ha ha ha ha ha !!!"
+}
+
+
+
+
+/*DRAFT*/
+/* function changeElement(selector,property,value){
+    let change = `document.querySelector("${selector}").style.${property} = "${value}";`
+    console.log(selector)
+    console.log(property)
+    console.log(value)
+    console.log(change)
+    return change
+} */
+/* function changeColor(){
+    document.querySelector('.new-section').style.backgroundColor = "#004a7f"
+} */
 /* function addButton() {
     userInput = prompt (message[4]);
     addElement("button", null,".conteneur")
